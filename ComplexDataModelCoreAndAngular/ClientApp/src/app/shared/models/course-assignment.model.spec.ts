ec1c23f068daf0a23984d2d9120b1f5036ff1a8c
MAzam211997 <mazam19971@gmail.com>
2021-07-10 19:25:05 +0500

    Added All Models Based On Entities

---
+++ ComplexDataModelCoreAndAngular/ClientApp/src/app/shared/models/course-assignment.model.spec.ts
@@ -0,0 +1,7 @@
+import { CourseAssignment } from './course-assignment.model';
+
+describe('CourseAssignment', () => {
+  it('should create an instance', () => {
+    expect(new CourseAssignment()).toBeTruthy();
+  });
+});
---
+++ ComplexDataModelCoreAndAngular/ClientApp/src/app/shared/models/office-assignment.model.spec.ts
@@ -0,0 +1,7 @@
+import { OfficeAssignment } from './office-assignment.model';
+
+describe('OfficeAssignment', () => {
+  it('should create an instance', () => {
+    expect(new OfficeAssignment()).toBeTruthy();
+  });
+});
