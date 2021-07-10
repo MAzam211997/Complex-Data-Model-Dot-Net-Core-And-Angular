af691a13e7575707444da4820c6a6f6cc4f29730
MAzam211997 <mazam19971@gmail.com>
2021-07-10 18:55:00 +0500

    Add project files.

---
+++ ComplexDataModelCoreAndAngular/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using App.Entities.Entities;
+using ComplexDataModelCoreAndAngular.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplexDataModelCoreAndAngular.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Course>().ToTable("Course");
+            //modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            //modelBuilder.Entity<Student>().ToTable("Student");
+            //modelBuilder.Entity<Department>().ToTable("Department");
+            //modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            //modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            //modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>().HasKey(c => new { c.CourseID, c.InstructorID });
+        }
+    }
+}
