ba4aa3e7529ab3ca8f834579b0ad94965e9bff2a
Muhammd Azam <mazam19971@gmail.com>
2021-09-24 20:31:54 +0500

    Added Dynamic Methods with Dynamic Return Type

---
+++ MethodPractices/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicMethods methods = new DynamicMethods();
+            Category category = new Category();
+            Product product = methods.GetDynamicData(category);
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class DynamicMethods
+    {
+      public  dynamic GetDynamicData(dynamic data)
+        {
+            return data;
+        }
+      public  List<dynamic> GetDynamicData(List<dynamic> data)
+        {
+            return data;
+        }
+      public  Dictionary<dynamic,dynamic> GetDynamicData(Dictionary<dynamic, dynamic> data)
+        {
+            return data;
+        }
+    }
+    public class Category
+    {
+        public int CategoryID { get; set; } = 1;
+        public String Name { get; set; } = "Laptops";
+    }
+    public class Product
+    {
+        public int ProductID { get; set; } = 1;
+        public String Name { get; set; } = "Dell Core i5";
+    }
+}
